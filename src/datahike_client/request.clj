8d32e7674cb7d315846d746968bb292ffad19530
Timo Kramer <fw-git@timokramer.de>
2021-06-07 10:25:19 +0200

    Format source

--- src/datahike_client/request.clj
+++ src/datahike_client/request.clj
@@ -10,19 +10,19 @@
           writer (transit/writer out :json)
           _ (transit/write writer params)
           request-map {:url (str (.endpoint client) uri)
-                      :method method
-                      :headers (merge headers
-                                      {"Accept" "application/transit+json"
+                       :method method
+                       :headers (merge headers
+                                       {"Accept" "application/transit+json"
                                         "Content-type" "application/transit+json"
                                         "Charset" "utf-8"}
-                                      (when-let [token (.token client)]
-                                        {"authorization" (str "token " token)}))
-                      :timeout (or timeout 600)
-                      :body (.toString out)
-                      :as :stream}
+                                       (when-let [token (.token client)]
+                                         {"authorization" (str "token " token)}))
+                       :timeout (or timeout 600)
+                       :body (.toString out)
+                       :as :stream}
           response @(http/request request-map)]
       (if (and (contains? response :status)
-                (< (:status response) 400))
+               (< (:status response) 400))
         (transit/read (transit/reader (:body response) :json))
         (throw (ex-info "Failed request" {:request request-map
                                           :response response}))))))
